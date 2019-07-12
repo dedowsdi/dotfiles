24e52a43ddbdd6dc052777b5d5bef57be2f54425
PntAndCnt <peanutandchestnut@gmail.com>
2019-07-12 17:23:23 +0800

    clear unused scripts

--- .script/buildctags.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-ctags -R --sort=foldcase --excmd=number --fields=ksSi --links=yes --language-force=c++ --c++-kinds=+p --c-kinds=+p --exclude=build --exclude=cmake --exclude=CMake
--- .script/xpaste.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# used to paste from xsel in tty
-DISPLAY=:0 xsel -o
